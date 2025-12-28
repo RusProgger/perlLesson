cc4bd1fabcaa9138aa1987f7daf050a6901a0844
RusProgger <ruslantimka92@gmail.com>
2025-12-28 17:00:09 +0200

    Работа с boolean в perl

---
+++ 2. Variable/boolean/boolean.pl
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+# Модуль для работы с boolean
+# По умолчанию модуль может быть не подключен, нужно установить через командную строку cpan boolean
+
+use boolean;
+
+my $isMarried = false;
+
+# print "$isMarried";
+
+if($isMarried) {
+    print "True\n";
+} else {
+    print "False";
+}
+
+
