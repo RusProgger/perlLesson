f23392a8d6d586df5dfa0f2c337f86d636082138
RusProgger <ruslantimka92@gmail.com>
2025-12-29 08:55:16 +0200

    Работа со строками в Perl

---
+++ 2. Variable/string/string.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use boolean;
+
+use feature "say"; # для say работает с версии 5.10 и выше
+
+my $string = "Hello";
+my $string2 = "World!";
+
+my $ch = 'Hell'; # Тоже строка
+
+
+
+print "$string $string2\n";
+printf("String  = %s \nString2 = %s\n", $string2, $string);
+
+print "$ch\n";
+
+say "$ch"; # say сам добавит новую строку
+
+
+
