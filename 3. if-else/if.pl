be0742345ea3c2eac085a9db68c733ab03f2e256
RusProgger <ruslantimka92@gmail.com>
2025-12-29 10:49:30 +0200

    Работа со строками в Perl

---
+++ 3. if-else/if.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+use boolean;
+use feature "say";
+
+my $x = 10000; 
+my $c = 100;
+
+if($x > $c) {
+    printf("Variable X: %d", $x);
+} else {
+    printf("Variable C: %d", $c);
+}
