19d4c00067de0ffa7d499441c9f5a9928b0af2f7
RusProgger <ruslantimka92@gmail.com>
2025-12-29 11:35:20 +0200

    Работа с тернарным оператором  в Perl

---
+++ 5. TernaryOperator/TernaryOperator.pl
@@ -0,0 +1,14 @@
+use strict;
+use warnings;
+
+my $a = 10;
+my $b = 100;
+
+($a > $b) ? print("A > B\n") : print("B > A\n");
+
+my $test = "Hello";
+my $cmp = "Denis";
+
+my $res = ($test eq $cmp) ? print($test) : print($cmp);
+
+print "\nInput = ", $res;
