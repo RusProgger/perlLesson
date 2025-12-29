d5bb1591cf3fa9a55cf6e0bdbfc8db7a1000587a
RusProgger <ruslantimka92@gmail.com>
2025-12-29 10:58:18 +0200

    Работа с условиями в Perl

--- 3. if-else/if.pl
+++ 3. if-else/if.pl
@@ -7,7 +7,24 @@ my $x = 10000;
 my $c = 100;
 
 if($x > $c) {
-    printf("Variable X: %d", $x);
+
+    printf("Variable X: %d\n", $x);
+
 } else {
-    printf("Variable C: %d", $c);
+
+    printf("Variable C: %d\n", $c);
+
 }
+
+
+my $number = 55;
+
+if($number > 100) {
+    print("Good");
+} elsif($number == 55) {
+    print("ok!");
+} else {
+    print("Error");
+}
+
+
