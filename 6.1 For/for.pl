de28670533678200377be554c3d5336ff28c32c0
RusProgger <ruslantimka92@gmail.com>
2026-01-03 03:30:07 +0200

    Работа с циклом for

--- 6.1 For/for.pl
+++ 6.1 For/for.pl
@@ -2,10 +2,10 @@ use strict;
 use warnings;
 
 # my $name = "Alex";
-my $count = 1;
+# my $count = 1;
 
 # for cycle
 
-for(; $count < 10; $count++) {
+for(my $count = 1; $count < 10; $count++) {
     print "Variable$count = {$count}\n";
 }
