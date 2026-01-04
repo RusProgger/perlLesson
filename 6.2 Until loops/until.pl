d666185958bbc9184b08cd50802be8aa759f2cc1
RusProgger <ruslantimka92@gmail.com>
2026-01-04 02:35:41 +0200

    Работа с циклом until

--- 6.2 Until loops/until.pl
+++ 6.2 Until loops/until.pl
@@ -4,7 +4,16 @@ use warnings;
 my $number = 1;
 
 until($number > 10) {
-    print "$number\n";
+    # print "$number\n";
+    # $number++;
+
+    if($number % 2 == 0) {
+        print "Even number = $number\n";
+    } 
+    #else {
+    #     print "Odd number = $number\n";
+    # }
+
     $number++;
 }
 
