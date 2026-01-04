6ebff4a3f173c43c29e48391b2f957eabd5fc4c1
RusProgger <ruslantimka92@gmail.com>
2026-01-04 03:10:27 +0200

    Работа с циклом until в PERL

--- 6.2 Until loops/until.pl
+++ 6.2 Until loops/until.pl
@@ -3,6 +3,7 @@ use warnings;
 
 my $number = 1;
 
+print "\n************** Even number **************\n";
 until($number > 10) {
     # print "$number\n";
     # $number++;
@@ -10,8 +11,16 @@ until($number > 10) {
     if($number % 2 == 0) {
         print "Even number = $number\n";
     } 
-    
+
     $number++;
 }
 
-print "\n************** Even number **************\n";
+
+
+print "\n************** Odd number ****************\n";
+
+for(my $n = 1; $n <= 10; $n++) {
+    if($n % 2 != 0) {
+        print "Odd number = $n\n";
+    }
+}
