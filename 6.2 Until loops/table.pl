a8b77c3de0fa9c5cbf4bc40e8d9015654ed56b93
RusProgger <ruslantimka92@gmail.com>
2026-01-08 01:14:59 +0200

    Практика таблица умножения, работа с циклом

--- 6.2 Until loops/table.pl
+++ 6.2 Until loops/table.pl
@@ -4,16 +4,30 @@ use warnings;
 
 # Таблица умножения 
 
-for(my $i = 1; $i <= 10; $i++) {
-    for(my $j = 1; $j <= 10; $j++) {
-        my $result = ($i * $j);
-        # print "$i * $j = ", $i * $j,  "\n";
-        # Добавляем условия столбцов
+# for(my $i = 1; $i <= 10; $i++) {
+#     for(my $j = 1; $j <= 10; $j++) {
+#         my $result = ($i * $j);
+#         # print "$i * $j = ", $i * $j,  "\n";
+#         # Добавляем условия столбцов
 
-        if($j == 3) {
-            print "\n"; 
+#          print "$i * $j = $result\t"; 
+#         if($j % 3 == 0) {
+#             print "\n"; 
+#         }
+       
+#     }
+#     print "\n";
+# }
+
+for (my $i = 1; $i <= 10; $i += 3) {
+    for (my $j = 1; $j <= 10; $j++) {
+        for (my $k = 0; $k < 3; $k++) {
+            my $col = $i + $k;
+            last if $col > 10; # не печатать, если больше 10
+            my $result = $col * $j;
+            printf "%2d * %2d = %3d\t", $col, $j, $result;
         }
-        print "$i * $j = " . $result; 
+        print "\n";
     }
     print "\n";
 }
