973fab15839f877d8ebfe18cef0eda712421c0f5
RusProgger <ruslantimka92@gmail.com>
2026-01-08 00:57:50 +0200

    Практика таблица умножения, работа с циклом

--- 6.2 Until loops/table.pl
+++ 6.2 Until loops/table.pl
@@ -4,3 +4,9 @@ use warnings;
 
 # Таблица умножения 
 
+for(my $i = 1; $i <= 10; $i++) {
+    for(my $j = 1; $j <= 10; $j++) {
+        print "$i * $j = ", $i * $j,  "\n";
+    }
+    print "\n";
+}
