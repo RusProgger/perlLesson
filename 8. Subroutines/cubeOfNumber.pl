44a4092177afed27a67e0a7c1f8b4bffcfed1cbc
RusProgger <ruslantimka92@gmail.com>
2026-01-15 07:04:10 +0200

    Практика, вычисление куба,  добавлена проверка.

--- 8. Subroutines/cubeOfNumber.pl
+++ 8. Subroutines/cubeOfNumber.pl
@@ -4,10 +4,17 @@ use warnings;
 sub cube {
     my $cube = $_[0];
     # return $cube * $cube * $cube;
+
+    # добивим проверку условий если число меньше 0
+
+    if($cube <= 0) {
+        die "Error!!\n";
+    }
+
     return $cube ** $cube;
 }
 
-my $result = cube(3);
+my $result = cube(0);
 
 # Вывод информации
 print "Result: " . $result;
