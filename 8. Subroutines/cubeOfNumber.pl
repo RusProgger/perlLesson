e9579167bae483a49ec61bbece716dc6b6361ed0
RusProgger <ruslantimka92@gmail.com>
2026-01-15 06:59:33 +0200

    Практика, вычисление куба

--- 8. Subroutines/cubeOfNumber.pl
+++ 8. Subroutines/cubeOfNumber.pl
@@ -3,10 +3,11 @@ use warnings;
 
 sub cube {
     my $cube = $_[0];
-    return $cube * $cube * $cube;
+    # return $cube * $cube * $cube;
+    return $cube ** $cube;
 }
 
 my $result = cube(3);
 
 # Вывод информации
-print $result;
+print "Result: " . $result;
