7368e88c236ae9858207a261104f73930fdd26f5
RusProgger <ruslantimka92@gmail.com>
2026-01-15 06:32:26 +0200

    Передача аргументов в подпрограммах

--- 8. Subroutines/passingArgumentsInSubroutines.pl
+++ 8. Subroutines/passingArgumentsInSubroutines.pl
@@ -4,13 +4,13 @@ use warnings;
 # Передача аргументов в подпрограммах 
 
 sub add {
-    $num1 = $_[0];
-    $num2 = $_[1];
-    $num3 = $_[3];
+    my $num1 = $_[0];
+    my $num2 = $_[1];
+    my $num3 = $_[2];
 
     print "Value1: $num1\n";
     print "Value2: $num2\n";
     print "Value3: $num3\n";
 }
 
-add();
+add(10, 15, 45);
